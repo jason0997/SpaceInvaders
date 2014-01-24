ebc19c3fbe40c7f5821b5feb20664f7314356c41
jason0997 <417703557@qq.com>
2014-01-24 01:45:52 -0500

    Initialization

---
+++ javascripts/helper.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function get_right_side(matrix, row , col){
+	for(i = row-1; i>=0; i--){
+		for(j = 0; j< col; j++){
+			if(matrix[i][j] == 1){
+				return (i+1);
+			}
+		}
+	}
+}
+
+function get_left_side(matrix, row , col){
+	for(i = 0; i<row; i++){
+		for(j = 0; j< col; j++){
+			if(matrix[i][j] == 1){
+				return (i);
+			}
+		}
+	}
+}
