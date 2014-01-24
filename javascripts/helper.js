57e5f6b4879aefd98582d0df12029c6460d37dfc
jason0997 <417703557@qq.com>
2014-01-24 11:00:21 -0500

    Player dead condition done.

--- javascripts/helper.js
+++ javascripts/helper.js
@@ -18,3 +18,13 @@ function get_left_side(matrix, row , col){
 		}
 	}
 }
+
+function get_down_side(matrix, row , col){
+	for(i = row-1; i>=0; i--){
+		for(j = 0; j< col; j++){
+			if(matrix[j][i] == 1){
+				return (i+1);
+			}
+		}
+	}
+}
