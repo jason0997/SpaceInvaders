c740558303ea8474b7d69db2e9ef7a2af9bab0ec
jason0997 <417703557@qq.com>
2014-01-29 01:58:30 -0500

    Score and health canavas added, score done, logic for collision fixed.

--- javascripts/helper.js
+++ javascripts/helper.js
@@ -13,18 +13,34 @@ function get_left_side(matrix, row , col){
 	for(i = 0; i<row; i++){
 		for(j = 0; j< col; j++){
 			if(matrix[i][j] == 1){
-				return (i);
+				return (i+1);
 			}
 		}
 	}
 }
 
 function get_down_side(matrix, row , col){
-	for(i = row-1; i>=0; i--){
-		for(j = 0; j< col; j++){
-			if(matrix[j][i] == 1){
-				return (i+1);
+	for(j = col - 1; j >= 0; j--){
+		for(i = 0; i < row; i++){
+			if(matrix[i][j] == 1){
+				return (j+1);
 			}
+		}
+	}
+}
+
+function get_down_side_right(matrix, row, down){
+	for(i = row-1; i>=0; i--){
+		if(matrix[i][down-1] == 1){
+			return (i+1);	
+		}
+	}
+}
+
+function get_down_side_left(matrix, row, down){
+	for(i = 0; i<row; i++){
+		if(matrix[i][down-1] == 1){
+			return (i+1);	
 		}
 	}
 }
